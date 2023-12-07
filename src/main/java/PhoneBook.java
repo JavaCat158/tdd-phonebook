ea9e3c9d6923c801990ee72ffcfdc95b11fa168e
Ildar <ibragimovildar229@gmail.com>
2023-12-07 21:21:22 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneBook {
+    private List<Contact> contacts;
+    PhoneBook(){
+        contacts = new ArrayList<>();
+    }
     public int add(String name, String number) {
-        return 0;
+        if (contacts.stream().anyMatch(contact -> contact.getName().equals(name))) {
+            return contacts.size();
+        }
+
+        Contact newContact = new Contact(name, number);
+        contacts.add(newContact);
+
+        return contacts.size();
     }
 }
